941a7037e20a452b601acd02e1fdbd57a6879577
Chris Williams <chris@christopher-williams.net>
2012-09-15 17:12:47 -0400

    Fix the salt installation

--- fabfile.py
+++ fabfile.py
@@ -5,9 +5,8 @@ def prepare_ppa():
 	Gets the saltstack/salt PPA ready for use
 	"""
 	sudo('apt-get install -y python-software-properties')
-	sudo("add-apt-repository ppa:saltstack/salt -y")
+	sudo("add-apt-repository ppa:saltstack/salt-daily -y")
 	sudo("apt-get update -y")
-	sudo('adduser --no-create-home --disabled-login --system salt')
 
 def bootstrap_minion():
 	"""
@@ -15,7 +14,7 @@ def bootstrap_minion():
 	"""
 	execute(prepare_ppa)
 	# Install the minion
-	sudo("apt-get install salt-minion -y")
+	sudo("apt-get install salt-minion -y --force-yes")
 	sudo('service salt-minion stop')
 
 	put('configs/salt-minion.conf', '/etc/salt/minion', use_sudo=True)
@@ -26,8 +25,9 @@ def bootstrap_master():
 	Install the salt master
 	"""
 	execute(prepare_ppa)
-	sudo('apt-get install salt-master -y')
+	sudo('apt-get install salt-master -y --force-yes')
 	sudo('service salt-master stop')
 	
 	put('configs/salt-master.conf', '/etc/salt/master', use_sudo=True)
+	sudo('mkdir -p /srv/salt/states')
 	sudo('service salt-master start')
